9d1ac371493b73d6bc6a463abc07c559e8358521
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-06-24 21:29:31 +0530

    border chngd for main__cup

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let main__cup__fill = document.getElementById("main__cup__fill");
+const main__cup__empty = document.getElementById("main__cup__empty");
 let cupsContainer = document.getElementById("cups");
 let cups = document.querySelectorAll(".cup");
 let currentActiveCup = 0;
 
 cupsContainer.addEventListener("click", (e) => {
     console.log("inside cups container");
+
     if (e.target.dataset.index) {
         currentActiveCup = parseInt(e.target.dataset.index);
         updateDOMCups();
@@ -38,11 +40,18 @@ function updateDOMCupsClear() {
 
 function updateDOMMainCup() {
     let percentage = (currentActiveCup / cups.length) * 100;
+    main__cup__empty.style.height = `${100 - percentage}%`;
+    main__cup__empty.style.opacity = 1;
+
     main__cup__fill.style.height = `${percentage}%`;
+    main__cup__fill.innerText = `${percentage}%`;
+    main__cup__empty.innerText = `${(100 - percentage) / 100 * 2}L`;
+    
+
 
 }
 
 function updateDOMMainCupClear() {
-    let percentage = (currentActiveCup / cups.length) * 100;
-    main__cup__fill.style.height = `${percentage}%`;
+    main__cup__fill.style.height = '0%';
+    main__cup__empty.style.height = '0%';
 }
